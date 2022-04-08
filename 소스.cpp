9461c9deab3419fcd83bc140ee3eaf70c6ee6e78
J.H. Lee <81499926+c8a4b5@users.noreply.github.com>
2022-04-08 10:36:04 +0900

    Add files via upload

---
+++ 소스.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void multab() {
+	for (int ind = 1; ind <= 8; ind++) {
+		cout << "--------" << ind << "´Ü" << "--------" << endl;
+		for (int ind2 = 1; ind2 <= 9; ind2++) {
+			cout << setw(4) << ind << setw(4) << "*" << setw(4) << ind2 << setw(4) << "=" << setw(4) << ind * ind2 << endl;
+		}
+	}
+}
+
+int main() {
+	multab();
+}
